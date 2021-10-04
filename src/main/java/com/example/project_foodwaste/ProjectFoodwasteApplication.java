0413b959198f4224b2e11cba9d4511597f38c84b
MaltheGram <malthegram22@gmail.com>
2021-10-04 10:16:48 +0200

    first commit

---
+++ src/main/java/com/example/project_foodwaste/ProjectFoodwasteApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_foodwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFoodwasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFoodwasteApplication.class, args);
+    }
+
+}
