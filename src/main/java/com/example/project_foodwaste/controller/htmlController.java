c6e6f64571a8380ff4b97c80b242f1b83d325f7e
Simon Gredal <simon.gredal@icloud.com>
2021-10-08 11:37:27 +0200

    Make htmlController less insane

--- src/main/java/com/example/project_foodwaste/controller/htmlController.java
+++ src/main/java/com/example/project_foodwaste/controller/htmlController.java
@@ -3,25 +3,15 @@ package com.example.project_foodwaste.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class htmlController {
 
-
-
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index(){
         return "index";
-    }
-
-    @GetMapping("/info")
-    public String info(){
-        return "info";
-    }
-    @GetMapping("/text.html")
-    public String text(){
-        return "text";
     }
 
     @GetMapping("/templates/petition_submissions")
@@ -29,21 +19,10 @@ public class htmlController {
         return "petition_submissions";
     }
 
-    @GetMapping("/responsibility")
-    public String responsibility(){
-        return "responsibility";
-    }
-    @GetMapping("/what_we_do")
-    public String whatWeDo(){
-        return "what_we_do";
-    }
-    @GetMapping("/profile")
-    public String profile(){
-        return "profile";
-    }
-    @GetMapping("/credits")
-    public String credits(){
-        return "credits";
+    @GetMapping("/{path}")
+    public String genericTemplateReturner(@PathVariable String path) {
+        System.out.println(path);
+        return path;
     }
 
 
