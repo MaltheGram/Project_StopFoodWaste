8afdb9268359d118ea9d35ef43ac8ca7507fd0fc
Simon Gredal <simon.gredal@icloud.com>
2021-10-08 12:32:55 +0200

    Make htmlController less insane, again.

--- src/main/java/com/example/project_foodwaste/controller/htmlController.java
+++ src/main/java/com/example/project_foodwaste/controller/htmlController.java
@@ -16,58 +16,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class htmlController {
 
-
-
-    @GetMapping("/index")
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping("/info")
-    public String info() {
-        return "info";
-    }
-
-    @GetMapping("/text.html")
-    public String text() {
-        return "text";
-    }
-
-
-
     @GetMapping("/templates/petition_submissions")
-
     public String petition(@RequestParam String fName, @RequestParam String lName) {
         System.out.println(fName + " " + lName);
         return "petition_submissions";
     }
 
-
-    @GetMapping("/responsibility")
-    public String responsibility() {
-        return "responsibility";
-    }
-
-    @GetMapping("/what_we_do")
-    public String whatWeDo() {
-        return "what_we_do";
-    }
-
-    @GetMapping("/profile")
-    public String profile() {
-        return "profile";
-    }
-
-    @GetMapping("/credits")
-    public String credits() {
-        return "credits";
-    }
-
-        @GetMapping("/{path}")
-        public String genericTemplateReturner (@PathVariable String path){
-            System.out.println(path);
-            return "path";
-
-        }
+    @GetMapping("/{path}")
+    public String genericTemplateReturner (@PathVariable String path){
+        System.out.println(path);
+        return path;
 
     }
+
+}
