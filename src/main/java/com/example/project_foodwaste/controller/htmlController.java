3539a9d6005a33532d3c92c636dd0e8f04789fdf
MaltheGram <malthegram22@gmail.com>
2021-10-08 11:46:47 +0200

    fixed merge

--- src/main/java/com/example/project_foodwaste/controller/htmlController.java
+++ src/main/java/com/example/project_foodwaste/controller/htmlController.java
@@ -5,18 +5,18 @@ import com.example.project_foodwaste.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-=======
+
 import org.springframework.web.bind.annotation.PathVariable;
->>>>>>> c6e6f64571a8380ff4b97c80b242f1b83d325f7e
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class htmlController {
 
-<<<<<<< HEAD
+
 
     @GetMapping("/index")
     public String index() {
@@ -33,20 +33,21 @@ public class htmlController {
         return "text";
     }
 
-=======
+
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
->>>>>>> c6e6f64571a8380ff4b97c80b242f1b83d325f7e
     @GetMapping("/templates/petition_submissions")
+
     public String petition(@RequestParam String fName, @RequestParam String lName) {
         System.out.println(fName + " " + lName);
         return "petition_submissions";
     }
 
-<<<<<<< HEAD
+
     @GetMapping("/responsibility")
     public String responsibility() {
         return "responsibility";
@@ -65,15 +66,15 @@ public class htmlController {
     @GetMapping("/credits")
     public String credits() {
         return "credits";
-=======
-    @GetMapping("/{path}")
-    public String genericTemplateReturner(@PathVariable String path) {
-        System.out.println(path);
-        return path;
->>>>>>> c6e6f64571a8380ff4b97c80b242f1b83d325f7e
+
+        @GetMapping("/{path}")
+        public String genericTemplateReturner (@PathVariable String path){
+            System.out.println(path);
+            return path;
+
+        }
+
+
     }
-
-
-
 
 }
