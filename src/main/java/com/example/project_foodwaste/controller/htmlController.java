7e8038762e7ad20df4db7c5c3f38554e07c7f0f1
MaltheGram <malthegram22@gmail.com>
2021-10-09 22:48:32 +0200

    minor css changes to info page. Resized border radius and bg-color

--- src/main/java/com/example/project_foodwaste/controller/htmlController.java
+++ src/main/java/com/example/project_foodwaste/controller/htmlController.java
@@ -1,13 +1,8 @@
 package com.example.project_foodwaste.controller;
 
 
-import com.example.project_foodwaste.model.Person;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -46,6 +41,10 @@ public class htmlController {
     @GetMapping("/responsibility")
     public String responsibility() {
         return "responsibility";
+    }
+    @GetMapping("/test")
+    public String xxx() {
+        return "scroll";
     }
 
     @GetMapping("/what_we_do")
