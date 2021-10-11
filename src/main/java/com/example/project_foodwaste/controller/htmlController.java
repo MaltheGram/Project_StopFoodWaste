4e1b917df3d06949d60708a8c958a7d14ccd433c
MaltheGram <malthegram22@gmail.com>
2021-10-11 13:54:44 +0200

    minorchange and CSS clean up. Moved navigations to it's own CSS

--- src/main/java/com/example/project_foodwaste/controller/htmlController.java
+++ src/main/java/com/example/project_foodwaste/controller/htmlController.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class htmlController {
     @GetMapping("/templates/petition_submissions")
     public String petition(@RequestParam String fName, @RequestParam String lName) {
-        System.out.println(fName + " " + lName);
         return "petition_submissions";
     }
 
